585c761f000f498965ead8e7af6218ec6e94f5b3
Emill03 <emillsolano22@gmail.com>
2024-08-02 21:46:33 -0400

    commit

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kxb!rnbd1ni&*esisx(9(!ss_b6v1**wq@urq54bu6zy246$1v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
