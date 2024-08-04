224212c2970b523bec17e04e799d5c4397ff6259
Yordi <ys21-0834@unphu.edu>
2024-08-03 21:55:46 -0400

    V0.1

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from . import views
+from .views import CustomPasswordResetView
 
 urlpatterns = [
     path('member/', views.members, name='members'),
     path('', views.index, name='index'),
+    path('', views.home_view, name='home'),
+    path('privacy-policy/', views.privacy_policy_view, name='privacy_policy'),
+    path('contact/', views.contact_view, name='contact'),
+    path('news/', views.news_view, name='news'),
+    path('list/', views.list_view, name='list'),
+    path('login/', views.login_view, name='login'),
+     path('password-reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+
 ]
