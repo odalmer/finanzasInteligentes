a186426a49e0413fca12703df3e6394b859f32d9
Yordi <ys21-0834@unphu.edu>
2024-08-06 22:37:28 -0400

    v0.1.1.1.5

--- my_tennis_club/members/static/js/scripts.js
+++ my_tennis_club/members/static/js/scripts.js
@@ -1,3 +1,17 @@
+// scripts.js
+
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        localStorage.removeItem('darkMode');
+    }
 }
+
+// Check if dark mode is enabled on page load
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        document.body.classList.add('dark-mode');
+    }
+});
