8220d7f7955406b3146c8c31d2f6ad5c5e119a4f
odalmer <odalmerg4@gmail.com>
2024-08-10 19:08:06 -0400

    logout

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.signin, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
     path('detalles-ahorrar/', views.detalles_ahorrar, name='detalles_ahorrar'),
-    path('detalles-inversion/', views.detalles_inversion, name='detalles_inversion'),
+    path('detalles-inversion/', views.detalles_inversion,
+         name='detalles_inversion'),
     path('detalles-deuda/', views.detalles_deuda, name='detalles_deuda'),
-    path('detalles-planificacion/', views.detalles_planificacion, name='detalles_planificacion'),
-    path('detalles-jubilacion/', views.detalles_jubilacion, name='detalles_jubilacion'),
-    path('detalles-ahorro-inteligente/', views.detalles_ahorro_inteligente, name='detalles_ahorro_inteligente'),
+    path('detalles-planificacion/', views.detalles_planificacion,
+         name='detalles_planificacion'),
+    path('detalles-jubilacion/', views.detalles_jubilacion,
+         name='detalles_jubilacion'),
+    path('detalles-ahorro-inteligente/', views.detalles_ahorro_inteligente,
+         name='detalles_ahorro_inteligente'),
 ]
