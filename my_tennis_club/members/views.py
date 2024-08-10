68078ed7599c7cc19a7a25b35c901009ab28c458
Emill03 <emillsolano22@gmail.com>
2024-08-10 14:46:51 -0400

    consejos financieros

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -11,3 +11,20 @@ def login(request):
         pass
     return render(request, 'login.html')
 
+def detalles_ahorrar(request):
+    return render(request, 'detalles_ahorrar.html')
+
+def detalles_inversion(request):
+    return render(request, 'detalles_inversion.html')
+
+def detalles_deuda(request):
+    return render(request, 'detalles_deuda.html')
+
+def detalles_planificacion(request):
+    return render(request, 'detalles_planificacion.html')
+
+def detalles_jubilacion(request):
+    return render(request, 'detalles_jubilacion.html')
+
+def detalles_ahorro_inteligente(request):
+    return render(request, 'detalles_ahorro_inteligente.html')
